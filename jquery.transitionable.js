c813935b24445aec1faa622a453a1a71e63fabd0
root <root@srv-kano.intra.misiv.fr>
2016-04-25 14:12:53 +0200

    switched from jquery animation to pure CSS transitions

--- jquery.transitionable.js
+++ jquery.transitionable.js
@@ -1,7 +1,7 @@
 /**
- * Custom jquery plugin for easily navigate slideshow-like elements.
- * Could not find simple implementation for what I wanted so here it is...
- * TODO: instead of using jquery animations, use CSS animations as more performant (GPU internal stuff)
+ * Custom jquery plugin for easily navigate slideshow-like elements with either fading or translating effect.
+ * There are some implementations out there but are either too heavily-featured or do not provide both fading and translating.
+ * It uses CSS transitions internally.
  * 
  * Given an element, this plugin converts all its direct children as being transitionable like a slideshow.
  * I wanted simple "next/prev" methods to be called to transition from the current element to the next/previous sibliing.
@@ -17,12 +17,14 @@
  *      - loop: boolean true|false. If next is called when there are not next sibling in the DOM, the default is looping back to the previous element (default: true)
  *      - effect: string "fade"|"slide" or null. If no effect is specified when doing the transition, defaults to the one specified.
  *          By default, null is applied indicating we don't apply any effects.
- *      - fadeDuration: integer. Number of miliseconds for the fading effect. Defaults to jquery default (400ms).
- *      - slideDuration: integer. Numbert of miliseconds for the sliding effect. Defaults to jquery default (400ms).
  *      
  * Event:
  *      - fab-transitionablebeforeloop: when a call to next or prev leads to a loop to the opposite element, this event is fired.
  *                                      The user can cancel the loop effect by returning false.
+ *                                      
+ * By default, timing animation is set to 0.25s in the accompanying stylesheet.
+ * To override the default, simply add a CSS rule that override transition-duration property of the rules .page-fab-transitionable.slide-transition 
+ * or .page-fab-transitionable.fade-transition.
  * 
  * Usage examples:
  * This plugin support 2 invokation modes: via "named-method" call:
@@ -40,6 +42,8 @@
  * while an animation is running, is is simply ignored.
  * 
  * The plugin can be destroyed by calling the destroy method.
+ * 
+ * Inspiration: https://github.com/ccoenraets/PageSlider
  *
  * @author Lanoux Fabien
  */
@@ -63,8 +67,6 @@
     var defaults = {
         loop: true, // allow looping when end is bound limit is reached
         effect: null, // default effect
-        fadeDuration: $.fx.speeds._default, // default fading duration
-        slideDuration: $.fx.speeds._default // default sliding duration
     },
         pluginName = 'fab-transitionable',
         dataKey = 'plugin-' + pluginName // data key keeping the instance of the plugin to the wrapper element
@@ -84,45 +86,36 @@
         /**
          * Performs plugin initialization by keeping the width of the wrapper element (and adjusting it on resize)
          * and hide all other children but the first child.
-         * @returns {undefined}
          */
         init: function() {
             var wrapper = this.element,
-                children = wrapper.children(),
-                self = this;
+                children = wrapper.children();
         
             wrapper.addClass("wrapper-" + pluginName);
             this.index = 0;
-            this.width = wrapper.width();
-            children.not(":eq(0)").hide();
-            wrapper.on("resize", function () {
-                self.width = wrapper.width();
-            });
+
+            children.addClass("page-" + pluginName).not(":eq(0)").hide();
         },
         
         /**
          * Performs the actual transition using jquery animation.
          * 
          * @param {string} direction "prev"|"next"
-         * @param {string} effect "slide"|"fade"
+         * @param {string} effect (optional) "slide"|"fade"|callback
          * @param {function} complete (optional) - the callback to call when the transition is finished
-         * @returns {undefined}
          */
         _navigate: function (direction, effect, complete) {
             var wrapper = this.element,
                 children = wrapper.children(),
                 elt = children.eq(this.index),
-                siblings = children.not(elt),
                 beforeLoopEvent = $.Event(pluginName + "beforeloop")
                 ;
             
-            // does not support animation queuing
-            if (elt.is(":animated")) {
-                return;
+            if (typeof effect === "function") {
+                complete = effect;
+                effect = this.options.effect;
             }
-            
             complete = complete || $.noop;
-            effect = effect || this.options.effect;
         
             beforeLoopEvent.target = this.element; // to support delegated events
             
@@ -145,35 +138,49 @@
             
             // For any transition, once the effect is done we must have only 1 element visible (the others are hidden)
             switch (effect) {
-                // algo: stack all elements (in case coordinates changed after sliding effect)
-                // and fade-in the target element while fading out the current one (Note: fadeIn show a hidden element and fadeOut hide a visible element)
+                // opacity transition
                 case "fade":
-                    children.css("left", 0);
-                    target.fadeIn(this.options.fadeDuration);
-                    elt.fadeOut(this.options.fadeDuration, complete);
+                    // set target to being visible but with 0 opacity
+                    target.attr("class", "page-" + pluginName + " hide").show();
+                    // set current element to 1 opacity
+                    elt.attr("class", "page-" + pluginName + " show");
+                    
+                    target.one('transitionend', function () {
+                       elt.hide();
+                       complete();
+                    });
+                    
+                    // Force reflow. More information here: http://www.phpied.com/rendering-repaint-reflowrelayout-restyle/
+                    wrapper[0].offsetWidth;
+                    
+                    // execute opposite opacity transition on target and current
+                    target.attr("class", "page-" + pluginName + " fade-transition show");
+                    elt.attr("class", "page-" + pluginName + " fade-transition hide");
                     break;
                     
-                // algo: stack all siblings of the current element to the left or right of the current element (depending of direction)
-                // and slide the current element left or right while sliding the target element simmetrically left or right.
-                // The current element is hidden once finished and the target element must be shown before sliding.
+                // translateX transition
                 case "slide":
-                    siblings.css("left", this.width * (direction === "next" ? 1 : -1));
-                    elt.animate({left: this.width * (direction === "next" ? -1 : 1)}, {
-                        done: function () {
-                            $(this).hide();
-                        },
-                        duration: this.options.slideDuration
+                    // set target to its starting position offset and visible
+                    target.attr("class", "page-" + pluginName + " " + (direction === "next" ? "right" : "left")).show();
+                    
+                    target.one('transitionend', function () {
+                        elt.hide();
+                        complete();
                     });
-                    target.show().animate({left: 0}, {
-                        done: complete,
-                        duration: this.options.slideDuration
-                    });
+                    
+                    // Force reflow. More information here: http://www.phpied.com/rendering-repaint-reflowrelayout-restyle/
+                    wrapper[0].offsetWidth;
+                    
+                    // execute opposite translation along X-axis on target and current
+                    target.attr("class", "page-" + pluginName +  " slide-transition center");
+                    elt.attr("class", "page-" + pluginName + " slide-transition " + (direction === "next" ? "left" : "right"));
                     break;
                 
+                // no transition: immediate hide/show
                 default:
-                    children.css("left", 0);
-                    target.show();
-                    elt.hide();
+                    target.attr("class", "page-" + pluginName).show();
+                    elt.attr("class", "page-" + pluginName).hide();
+                    complete();
                     break;
             }
         },
@@ -202,9 +209,10 @@
          * Destroy the plugin so it can clean after itself
          */
         destroy: function () {
-            var wrapper = this.element;
-            wrapper.off("resize").removeClass("wrapper-" + pluginName);
-            wrapper.removeData(dataKey);
+            this.element.removeData(dataKey)
+                    .removeClass("wrapper-" + pluginName)
+                    .children()
+                        .removeClass("page-" + pluginName + " hide show center left right slide-transition fade-transition");
         }
     };
 
